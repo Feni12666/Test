4f2b952996de431f69816c547fd37bc18b4a9337
Feni12666 <mdonik1244@gmail.com>
2025-09-21 08:12:33 +0400

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -4,42 +4,69 @@ const fs = require("fs-extra");
 const moment = require("moment-timezone");
 
 module.exports.config = {
-    name: "admin",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "ULLASH", //don't change my credit 
-    description: "Show Owner Info",
-    commandCategory: "info",
-    usages: "",
-    cooldowns: 5
+  name: "admin",
+  version: "1.0.4",
+  hasPermssion: 0,
+  credits: "𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭",
+  description: "Show Admin Info with Language Option",
+  commandCategory: "info",
+  usages: "[en/bn]",
+  cooldowns: 5
 };
 
-module.exports.run = async function({ api, event }) {
-    var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+module.exports.run = async function({ api, event, args }) {
+  var time = moment().tz("Asia/Dhaka").format("DD/MM/YYYY hh:mm:ss A");
+  const lang = args[0] && args[0].toLowerCase() === "bn" ? "bn" : "en";
 
-    var callback = () => api.sendMessage({
-        body: `
-┏━━━━━━━━━━━━━━━━━━━┓
-┃      🌟 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 🌟      
-┣━━━━━━━━━━━━━━━━━━━┫
-┃ 👤 𝐍𝐚𝐦𝐞      : 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓
-┃ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫    : 𝐌𝐚𝐥𝐞
-┃ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧  : 𝐒𝐢𝐧𝐠𝐥𝐞
-┃ 🎂 𝐀𝐠𝐞       :  𝟐𝟓
-┃ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧  : 𝐈𝐬𝐥𝐚𝐦
-┃ 🏫 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐝𝐢𝐩𝐥𝐨𝐦𝐚 𝐢𝐧 𝐀𝐠𝐫𝐢𝐜𝐮𝐥𝐭𝐮𝐫𝐞
-┃ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬  : 𝐅𝐞𝐧𝐢
-┣━━━━━━━━━━━━━━━━━━━┫
-┃ 🎭 𝐓𝐢𝐤𝐭𝐨𝐤  : shahadat.0071
-┃ 🌐 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : https://www.facebook.com/100001088468923
-┣━━━━━━━━━━━━━━━━━━━┫
-┃ 🕒 𝐔𝐩𝐝𝐚𝐭𝐞𝐝 𝐓𝐢𝐦𝐞:  ${time}
-┗━━━━━━━━━━━━━━━━━━━┛
-        `,
-        attachment: fs.createReadStream(__dirname + "/cache/1.png")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
-  
-    return request(encodeURI(`https://graph.facebook.com/61572299956804/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
-        .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
-        .on('close', () => callback());
+  let message;
+  if(lang === "bn") {
+    message = `
+🌟━━━━━━━━━━━━━━━━🌟
+👑 এডমিন তথ্য 👑
+🌟━━━━━━━━━━━━━━━━🌟
+
+🧑 নাম        : 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓
+🚹 লিঙ্গ       : পুরুষ
+❤️ সম্পর্ক     : একক
+🎂 বয়স        : ২৫
+🕌 ধর্ম        : ইসলাম
+🏫 শিক্ষা      : কৃষিতে ডিপ্লোমা
+🏡 ঠিকানা     : ফেনী
+
+🎵 TikTok      : shahadat.0071
+🌐 Facebook    : https://www.facebook.com/100001088468923
+
+🕒 সময়        : ${time}
+🌟━━━━━━━━━━━━━━━━🌟
+`;
+  } else {
+    message = `
+🌟━━━━━━━━━━━━━━━━🌟
+👑 ADMIN DETAILS 👑
+🌟━━━━━━━━━━━━━━━━🌟
+
+🧑 Name      : 𝐒𝐇𝐀𝐇𝐀𝐃𝐀𝐓
+🚹 Gender    : Male
+❤️ Relationship : Single
+🎂 Age       : 25
+🕌 Religion  : Islam
+🏫 Education : Diploma in Agriculture
+🏡 Address   : Feni
+
+🎵 TikTok    : shahadat.0071
+🌐 Facebook  : https://www.facebook.com/100001088468923
+
+🕒 Updated Time : ${time}
+🌟━━━━━━━━━━━━━━━━🌟
+`;
+  }
+
+  var callback = () => api.sendMessage({
+    body: message,
+    attachment: fs.createReadStream(__dirname + "/cache/1.png")
+  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
+
+  return request(encodeURI(`https://graph.facebook.com/61572299956804/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
+    .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
+    .on('close', () => callback());
 };
